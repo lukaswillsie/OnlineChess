c0e51f7db893f5d049899b923834409596f1762c
Willsie Lukas Elliott <willsiel@teach.cs.utoronto.ca>
2020-05-24 21:14:57 -0300

    Initial commit of project. Work done so far is I've constructed the UI for the initial activity, the login activity. No back end yet.

---
+++ app/src/main/java/com/example/onlinechess/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.onlinechess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class LoginActivity extends AppCompatActivity {
+    private boolean progressOn = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText username = findViewById(R.id.username);
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    v.setAlpha(1);
+                }
+                else {
+                    v.setAlpha(0.5f);
+                }
+            }
+        });
+
+        EditText password = findViewById(R.id.password);
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    v.setAlpha(1);
+                }
+                else {
+                    v.setAlpha(0.5f);
+                }
+            }
+        });
+    }
+
+    public void stopBar(View button) {
+        if(progressOn) {
+            findViewById(R.id.progress_bar).setVisibility(View.GONE);
+        }
+        else {
+            findViewById(R.id.progress_bar).setVisibility(View.VISIBLE);
+        }
+        progressOn = !progressOn;
+    }
+}
