28e31dde8cc8aed4e90ba1c4674c492cba994154
Willsie Lukas Elliott <willsiel@teach.cs.utoronto.ca>
2020-06-12 20:29:43 -0300

    Moved CreateAccountActivity

--- app/src/main/java/com/lukaswillsie/onlinechess/activities/login/CreateAccountActivity.java
+++ app/src/main/java/com/lukaswillsie/onlinechess/activities/login/CreateAccountActivity.java
@@ -1,13 +1,20 @@
-package com.lukaswillsie.onlinechess;
+package com.lukaswillsie.onlinechess.activities.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.lukaswillsie.onlinechess.R;
 
 public class CreateAccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void create(View view) {
+
     }
 }
