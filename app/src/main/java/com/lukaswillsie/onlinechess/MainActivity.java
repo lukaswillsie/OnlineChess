f4d992f4c4e2d9cdb9b8378422a92a1e3424d406
Willsie Lukas Elliott <willsiel@teach.cs.utoronto.ca>
2020-06-19 14:12:25 -0300

    Made some small UI changes

--- app/src/main/java/com/lukaswillsie/onlinechess/MainActivity.java
+++ app/src/main/java/com/lukaswillsie/onlinechess/MainActivity.java
@@ -1,14 +1,20 @@
 package com.lukaswillsie.onlinechess;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.lukaswillsie.onlinechess.activities.InteriorActivity;
+
+public class MainActivity extends InteriorActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public String getTag() {
+        return "MainActivity";
     }
 }
