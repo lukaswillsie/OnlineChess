b3d3e86ffd3fcf2872639dddc6b6d67e3d7640f9
Lukas Willsie <lukaswillsie@gmail.com>
2020-06-24 21:23:40 -0300

    Tweaked the length requirements for usernames and passwords

--- app/src/main/java/com/lukaswillsie/onlinechess/activities/login/Format.java
+++ app/src/main/java/com/lukaswillsie/onlinechess/activities/login/Format.java
@@ -2,10 +2,10 @@ package com.lukaswillsie.onlinechess.activities.login;
 
 class Format {
     static boolean validUsername(String username) {
-        return 5 < username.length() && username.length() < 16 && username.indexOf(' ') == -1 && username.indexOf(' ') == -1;
+        return 4 < username.length() && username.length() < 16 && username.indexOf(' ') == -1 && username.indexOf(' ') == -1;
     }
 
     static boolean validPassword(String password) {
-        return 5 < password.length() && password.length() < 16 && password.indexOf(' ') == -1 && password.indexOf(' ') == -1;
+        return 4 < password.length() && password.length() < 16 && password.indexOf(' ') == -1 && password.indexOf(' ') == -1;
     }
 }
